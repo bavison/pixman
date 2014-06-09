04843d606af76475967df1d12638fcecad0511b0
Ben Avison <bavison@riscosopen.org>
2014-06-09 22:22:38 +0100

    lowlevel-blt-bench: Add in_n_8888 test

--- test/lowlevel-blt-bench.c
+++ test/lowlevel-blt-bench.c
@@ -630,6 +630,7 @@ tests_tbl[] =
     { "add_0565_2x10",         PIXMAN_r5g6b5,      0, PIXMAN_OP_ADD,     PIXMAN_null,     0, PIXMAN_x2r10g10b10 },
     { "add_2a10_2a10",         PIXMAN_a2r10g10b10, 0, PIXMAN_OP_ADD,     PIXMAN_null,     0, PIXMAN_a2r10g10b10 },
     { "in_n_8_8",              PIXMAN_a8r8g8b8,    1, PIXMAN_OP_IN,      PIXMAN_a8,       0, PIXMAN_a8 },
+    { "in_n_8888",             PIXMAN_a8r8g8b8,    1, PIXMAN_OP_IN,      PIXMAN_null,     0, PIXMAN_a8r8g8b8 },
     { "in_8_8",                PIXMAN_a8,          0, PIXMAN_OP_IN,      PIXMAN_null,     0, PIXMAN_a8 },
     { "in_8888_8",             PIXMAN_a8r8g8b8,    0, PIXMAN_OP_IN,      PIXMAN_null,     0, PIXMAN_a8 },
     { "src_n_2222",            PIXMAN_a8r8g8b8,    1, PIXMAN_OP_SRC,     PIXMAN_null,     0, PIXMAN_a2r2g2b2 },
