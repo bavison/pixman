829f3dcc242dbcebfb4b83b8ba72fcfde402cbe1
Ben Avison <bavison@riscosopen.org>
2014-07-30 16:44:33 +0100

    armv6: Add nearest-scaled-cover src_8888_8888 fast path
    Without this patch, any such operations are matched against the fast path
    implementation in pixman-fast-path.c before general_composite_rect(), so
    we never get to use the armv6-optimised assembly fetcher routines.
    
    This patch adds a C wrapper to the same assembly routine used for the
    nearest-scaled-cover fetcher, adapted to perform a 2D plot rather than a
    single scanlne. The C is macroised so that later patches can use the same
    approach to build more complex fast paths from combinations of armv6
    fetcher/combiner/writeback routines in a similar manner to
    pixcman_composite_rect().
    
    lowlevel-blt-bench -n src_8888_8888:
    
         Before          After
         Mean   StdDev   Mean   StdDev  Confidence  Change
    L1   117.2  1.6      79.2   1.1     100.0%      -32.4%
    L2   44.1   3.1      49.9   2.4     100.0%      +13.2%
    M    40.0   0.1      72.5   0.1     100.0%      +81.4%
    HT   20.1   0.1      29.5   0.3     100.0%      +46.5%
    VT   19.4   0.1      27.7   0.2     100.0%      +42.7%
    R    18.2   0.1      26.2   0.2     100.0%      +44.1%
    RT   8.7    0.2      10.0   0.2     100.0%      +15.8%
    
    affine-bench * 0 0 1 src a8r8g8b8 a8r8g8b8:
    
         Before          After
         Mean   StdDev   Mean   StdDev  Confidence  Change
    0.5  46.6   0.1      110.5  0.1     100.0%      +137.2%
    0.75 39.1   0.1      88.5   0.1     100.0%      +126.1%
    1.0  36.3   0.2      71.7   0.1     100.0%      +97.7%
    1.5  26.7   0.1      55.3   0.1     100.0%      +106.8%
    2.0  19.9   0.0      43.5   0.0     100.0%      +119.2%

--- pixman/pixman-arm-common.h
+++ pixman/pixman-arm-common.h
@@ -469,6 +469,15 @@ cputype##_combine_##name##_u (pixman_implementation_t *imp,                   \
      FAST_PATH_X_UNIT_POSITIVE            |                             \
      FAST_PATH_Y_UNIT_ZERO)
 
+#define PIXMAN_ARM_NEAREST_SCALED_COVER_SRC_DST_FAST_PATH(cputype,op,s,d,func) \
+    {   PIXMAN_OP_ ## op,                                                      \
+        PIXMAN_ ## s,                                                          \
+        PIXMAN_ARM_NEAREST_SCALED_COVER_FLAGS,                                 \
+        PIXMAN_null, 0,                                                        \
+        PIXMAN_ ## d, FAST_PATH_STD_DEST_FLAGS,                                \
+        cputype ## _composite_nearest_scaled_cover_ ## func                    \
+    }
+
 #define PIXMAN_ARM_BIND_GET_SCANLINE(cputype, name)                         \
 void                                                                        \
 pixman_get_scanline_##name##_asm_##cputype (int32_t        w,               \
