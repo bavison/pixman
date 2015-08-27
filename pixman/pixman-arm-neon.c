59bdd89e1af4bc3bb89088dced117f4858b9f91a
Ben Avison <bavison@riscosopen.org>
2015-08-27 21:38:47 +0100

    armv7: Add SRC combiner
    The without-mask case reduces, as in the pixman-combine32.c version, to a
    trivial memcpy call, but the with-mask case benefits from assembly code.
    
    lowlevel-blt-bench results for src_8888_8_8888 (compared to ARMv6 version):
    
           Before          After
          Mean StdDev     Mean StdDev   Confidence   Change
    L1    67.5   0.40     92.1   0.38    100.00%     +36.3%
    L2    66.2   0.32     86.0   0.57    100.00%     +29.9%
    M     60.9   0.49     59.9   1.04     99.95%      -1.6%
    HT    34.6   0.34     33.1   0.37    100.00%      -4.3%
    VT    29.8   0.23     27.9   0.34    100.00%      -6.2%
    R     27.9   0.15     24.2   0.28    100.00%     -13.3%
    RT    12.0   0.11     10.7   0.14    100.00%     -11.2%
    
    lowlevel-blt-bench results for src_0565_8_8888 (compared to ARMv6 version):
    
           Before          After
          Mean StdDev     Mean StdDev   Confidence   Change
    L1    39.8   0.18     46.9   0.34    100.00%     +18.0%
    L2    39.3   0.02     46.6   0.23    100.00%     +18.5%
    M     38.3   0.36     43.4   0.39    100.00%     +13.3%
    HT    25.0   0.27     27.1   0.26    100.00%      +8.3%
    VT    22.5   0.23     24.9   0.24    100.00%     +10.9%
    R     21.6   0.24     23.0   0.22    100.00%      +6.3%
    RT    10.0   0.15     10.0   0.11     31.52%      -0.2%  (insignificant)
    
    lowlevel-blt-bench results for src_x888_8_8888 (compared to ARMv6 version):
    
           Before          After
          Mean StdDev     Mean StdDev   Confidence   Change
    L1    48.8   0.42     60.5   0.50    100.00%     +23.9%
    L2    47.9   0.22     58.5   0.39    100.00%     +22.1%
    M     43.2   0.28     49.1   0.57    100.00%     +13.6%
    HT    24.6   0.23     26.7   0.09    100.00%      +8.5%
    VT    21.2   0.15     23.1   0.30    100.00%      +9.0%
    R     19.6   0.12     20.7   0.03    100.00%      +5.5%
    RT     8.9   0.10      8.9   0.04     81.23%      -0.4%  (insignificant)
    
    lowlevel-blt-bench results for src_8888_8888_8888 (compared to ARMv6 version):
    
           Before          After
          Mean StdDev     Mean StdDev   Confidence   Change
    L1    80.3   0.43    114.1   0.75    100.00%     +42.2%
    L2    78.0   0.45     94.5   0.58    100.00%     +21.2%
    M     51.9   0.54     33.8   0.03    100.00%     -34.8%
    HT    34.2   0.39     26.2   0.02    100.00%     -23.3%
    VT    31.2   0.04     24.5   0.03    100.00%     -21.6%
    R     28.5   0.08     22.7   0.01    100.00%     -20.2%
    RT    14.1   0.15     11.6   0.02    100.00%     -17.4%

--- pixman/pixman-arm-neon.c
+++ pixman/pixman-arm-neon.c
@@ -424,6 +424,26 @@ static const pixman_fast_path_t arm_neon_fast_paths[] =
     { PIXMAN_OP_NONE },
 };
 
+void
+pixman_composite_scanline_src_mask_asm_neon (int32_t         w,
+                                             uint32_t       *dst,
+                                             const uint32_t *src,
+                                             const uint32_t *mask);
+
+static void
+neon_combine_src_u (pixman_implementation_t *imp,
+                    pixman_op_t              op,
+                    uint32_t *               dest,
+                    const uint32_t *         src,
+                    const uint32_t *         mask,
+                    int                      width)
+{
+    if (mask)
+        pixman_composite_scanline_src_mask_asm_neon (width, dest, src, mask);
+    else
+        memcpy (dest, src, width * sizeof (uint32_t));
+}
+
 PIXMAN_ARM_BIND_COMBINE_U (neon, over)
 PIXMAN_ARM_BIND_COMBINE_U (neon, add)
 PIXMAN_ARM_BIND_COMBINE_U (neon, out_reverse)
@@ -434,6 +454,7 @@ _pixman_implementation_create_arm_neon (pixman_implementation_t *fallback)
     pixman_implementation_t *imp =
 	_pixman_implementation_create (fallback, arm_neon_fast_paths);
 
+    imp->combine_32[PIXMAN_OP_SRC] = neon_combine_src_u;
     imp->combine_32[PIXMAN_OP_OVER] = neon_combine_over_u;
     imp->combine_32[PIXMAN_OP_ADD] = neon_combine_add_u;
     imp->combine_32[PIXMAN_OP_OUT_REVERSE] = neon_combine_out_reverse_u;
